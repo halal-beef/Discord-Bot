1a844c44da10222eab1830d585beb99e37a5bfe9
usrDottik <dotcomnose@gmail.com>
2022-01-15 12:53:50 -0300

    Add basic stuff.

---
+++ Discord Bot/Libraries.cs
@@ -0,0 +1,12 @@
+﻿global using System;
+
+global using System.Linq;
+global using System.Text;
+
+global using System.Threading.Tasks;
+global using System.Collections.Generic;
+
+//NuGet Packages
+global using Discord;
+global using Discord.Rest;
+global using Discord.WebSocket;
--- Discord Bot/Program.cs
+++ Discord Bot/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace Rocket.Main
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
+	public class Program
+	{
+		private static string Token = ""; //DISCORD BOT TOKEN HERE.
+		public static DiscordSocketClient _client;
 
-        }
-    }
+		public static Task Main(string[] args) => new Program().MainAsync();
+
+		public async Task MainAsync()
+		{
+			//Make Logs!1
+			_client.Log += Log;
+
+			//Start the Bot!
+			await _client.LoginAsync(TokenType.Bot, Token);
+			await _client.StartAsync();
+
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+		}
+		
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
 }
